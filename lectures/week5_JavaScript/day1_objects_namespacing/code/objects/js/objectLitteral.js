c83b4798977384fbc5bc01787879dadbe2389de0
Tom Blackmore <tablackmore@gmail.com>
2013-09-23 12:18:35 +0200

    added completed exercise

--- lectures/week5_JavaScript/day1_objects_namespacing/code/objects/js/objectLitteral.js
+++ lectures/week5_JavaScript/day1_objects_namespacing/code/objects/js/objectLitteral.js
@@ -1,3 +1,41 @@
 /*jslint browser:true */
 /*global alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
-console.log();
+console.log("objects");
+var jesperCat = {
+    "color": "Red",
+    "age": 200,
+    "favFood": "Tuna",
+    "name": "Jesper",
+    "makeSound": function () {
+        "use strict";
+        console.log("Meoow");
+    },
+    "weight": {
+        "value": 200,
+        "unit": "Tons"
+    }
+};
+var tomCat = {};
+tomCat.name = "Tom";
+tomCat.color = "Black/White";
+tomCat.makeSound = function () {
+    "use strict";
+    console.log("Core blimey guv, wud you like sum tea?");
+};
+var busterCat = {};
+busterCat["name"] = "Buster";
+busterCat["color"] = "White";
+busterCat["makeSound"] = function () {
+    "use strict";
+    console.log("Rooarrrrrrarrara");
+};
+var catFactory = function (name, color, sound) {
+    "use strict";
+    var that = {};
+    that.name = name;
+    that.color = color;
+    that.makeSound = function () {
+        console.log(sound);
+    };
+    return that;
+};
