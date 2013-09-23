6ee4c863c1bc421a8d8b46c9d92896904f61173e
Tom Blackmore <tablackmore@gmail.com>
2013-09-23 12:19:24 +0200

    added completed exercise

---
+++ lectures/week5_JavaScript/day1_objects_namespacing/code-completed/namespacing/js/triv.data.js
@@ -0,0 +1,16 @@
+/*jslint browser:true */
+/*global js:false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+triv.data = {};
+triv.data.questions = [{
+    q: "What is the capital of Sweden? \n 1: Stockholm \n 2: London \n 3. Helsinkini",
+    a: 1
+}, {
+    q: "What is the capital of UK? \n 1: Stockholm \n 2: London \n 3. Helsinkini",
+    a: 2
+}, {
+    q: "What is the capital of Finland? \n 1: Stockholm \n 2: London \n 3. Helsinkini",
+    a: 3
+}, {
+    q: "What is the capital of France? \n 1: Paris \n 2: Madrid \n 3. Helsinkini",
+    a: 1
+}];
