4d2443f47b55dccd3807b010daa03cf38d2e3d30
Tom Blackmore <tablackmore@gmail.com>
2013-10-07 08:40:53 +0200

    Added a little jQuery exercise

---
+++ lectures/week7_jquery/exercise/shoppingList/script.js
@@ -0,0 +1,7 @@
+/*jslint browser:true */
+/*global $: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+// 1) Listen to the click event on the span with the id of button
+// 2) When you click on it retrieve the value from the input element and 
+// add it to the unordered list as a "li" element
+// 3) Can you add a remove button beside each list item that will remove it from the list if clicked?
+//
