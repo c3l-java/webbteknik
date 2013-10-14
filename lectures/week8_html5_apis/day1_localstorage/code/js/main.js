c2922f87dd432695568bf9380a755c27f36398ed
Tom Blackmore <tablackmore@gmail.com>
2013-10-14 10:59:52 +0200

    added my code along for localstorage

---
+++ lectures/week8_html5_apis/day1_localstorage/code/js/main.js
@@ -0,0 +1,12 @@
+/*jslint browser:true */
+/*global localStorage: false, alert: false, confirm: false, console: false, Debug: false, opera: false, prompt: false, WSH: false */
+// Create an item in localStorage
+localStorage.setItem("username", "Tom");
+localStorage.password = "enrfjnjvnr";
+// Retieve an itemfrom localStorage
+localStorage.getItem("username");
+localStorage.username;
+// Remove an item from localStorage
+localStorage.removeItem("username");
+// Remove all items from localStorage
+localStorage.clear();
