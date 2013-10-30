1d7652c9ad13908ad90cb7339826229fd1a0602b
Tom Blackmore <tablackmore@gmail.com>
2013-10-30 10:10:19 +0100

    added spotify ajax

--- lectures/week10_http/day3_http/code/spotifyRemote/public/js/spotify.js
+++ lectures/week10_http/day3_http/code/spotifyRemote/public/js/spotify.js
@@ -3,21 +3,58 @@
 var spotify = {};
 spotify.play = function (callback) {
     "use strict";
+    $.ajax({
+        url: "/play",
+        type: "post"
+    }).done(function () {
+        if (callback !== undefined) {
+            callback();
+        }
+    });
 };
 spotify.pause = function (callback) {
     "use strict";
+    $.ajax({
+        url: "/pause",
+        type: "post"
+    }).done(function () {
+        if (callback !== undefined) {
+            callback();
+        }
+    });
 };
 spotify.next = function (callback) {
     "use strict";
+    $.ajax({
+        url: "/next",
+        type: "post"
+    }).done(function () {
+        if (callback !== undefined) {
+            callback();
+        }
+    });
 };
 spotify.previous = function (callback) {
     "use strict";
+    $.ajax({
+        url: "/previous",
+        type: "post"
+    }).done(function () {
+        if (callback !== undefined) {
+            callback();
+        }
+    });
 };
 spotify.getTrackName = function (id, callback) {
     "use strict";
 };
 spotify.loadTrack = function (text, callback) {
     "use strict";
+    $.ajax({
+        url: "http://ws.spotify.com/search/1/track.json?q=" + text
+    }).done(function (data) {
+        callback(data.tracks[0].href);
+    });
 };
 spotify.getArtwork = function (callback) {
     "use strict";
